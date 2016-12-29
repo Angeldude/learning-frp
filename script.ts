43053b84687e27e5e7657338829d8a1a3e729ae5
Angel Vanegas <angelman@gmail.com>
2016-12-28 23:56:14 -0500

    adding of 2 cells

---
+++ script.ts
@@ -0,0 +1,35 @@
+import { Cell, StreamSink} from 'sodiumjs';
+
+function currentText(input){
+  var sKeyPress = new StreamSink<string>(),
+  text = sKeyPress.hold(input.value);
+
+  input.addEventListener('keyup', function(e){
+    sKeyPress.send(input.value);
+  })
+  return text;
+}
+
+enum Key {
+  ONE, TWO
+}
+
+
+
+(function(){
+  var a = currentText(<HTMLInputElement>document.getElementById('a'))
+             .map(txt => parseInt(txt));
+  var b = currentText(<HTMLInputElement>document.getElementById('b'))
+            .map(txt => parseInt(txt));
+  var cSpan = document.getElementById('c');
+  var c = a.lift(b, function(x,y){
+    return x + y;
+  })
+
+  c.listen(z => {
+    isNaN(z) ?
+      cSpan.innerHTML = "" :
+      cSpan.innerHTML = (z).toString();
+  })
+
+})()
