961306ea0d31d397a92f721f731492c1e2533bba
Angel Vanegas <angelman@gmail.com>
2016-12-29 14:10:59 -0500

    making changes to display

--- display.ts
+++ display.ts
@@ -3,6 +3,8 @@ class Display {
     stream.listen(num => {
       if(input.value === "0")
         input.value = num.toString()
+      else if(num === "")
+        input.value = "0";
       else
         input.value += num.toString();
     });
