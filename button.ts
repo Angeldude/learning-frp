3dca08206ac1ed583424c539d6547c0c0d9c95bb
Angel Vanegas <angelman@gmail.com>
2016-12-31 09:15:21 -0500

    Update button.ts

--- button.ts
+++ button.ts
@@ -3,7 +3,6 @@ import { Cell } from 'sodiumjs';
 class SButton {
 
     private btn: HTMLButtonElement;
-    private listener;
     constructor(id: string, enabled = new Cell<boolean>(true)) {
         this.btn = <HTMLButtonElement>document.getElementById(id);
         this.btn.addEventListener('click', e => alert("appointment made!"))
