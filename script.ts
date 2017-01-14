6ee1db36cb2a3c4fda216d7d77b60774d9bf6068
Angel Vanegas <angelman@gmail.com>
2017-01-13 21:40:33 -0500

    complete translator

--- script.ts
+++ script.ts
@@ -2,15 +2,13 @@ import Button from './button';
 import Label from './label';
 import TextField from './stextfield';
 
-function translate(text: string): string{
-  return text.split(' ').map(e => e + "us").join(' ');
+const translate = function(unit, text: string): string{
+  return text === "" ? "" : text.split(' ').map(txt => txt + "us").join(' ');
 }
 
 const translateButton = new Button('trans');
 const textField = new TextField('text');
-const sLatin = translateButton.stream.snapshot(textField.text, (u,t)=> {
-  return translate(t);
-});
+const sLatin = translateButton.stream.snapshot(textField.text, translate);
 
 const latin = sLatin.hold('');
 
